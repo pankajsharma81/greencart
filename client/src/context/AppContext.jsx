08b0ec7a7665547ea1df9b973f82b54e27739f90
pankajsharma81 <pankajsharma88279@gmail.com>
2025-05-25 10:39:10 +0530

    added Navbar

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,27 @@
+// create context
+
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+// Provide Context Value (Wrap main Component)
+
+export const AppContextProvider = ({children}) =>{
+    
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null)
+    const [isSeller, setIsSeller] = useState(false)
+
+    const value = {navigate,user,setUser,isSeller,setIsSeller}
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+
+// consume context value
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
