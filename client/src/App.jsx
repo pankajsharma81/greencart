45af4570396c28a2b163a54e71b9a1d711f27e4a
pankajsharma81 <pankajsharma88279@gmail.com>
2025-11-13 09:16:40 +0530

    Add Footer component and integrate it into App; format code for consistency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -11,7 +12,7 @@ const App = () => {
     <div>
       {isSellerPath ? null : <Navbar />}
 
-      <Toaster/>
+      <Toaster />
       <div
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
       >
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 };
