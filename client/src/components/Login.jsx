1a45914b2849f95e79c367f4c087f730accc0e72
pankajsharma81 <pankajsharma88279@gmail.com>
2025-11-14 08:53:19 +0530

    Add AllProducts page and integrate it into App

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { useAppContext } from "../context/AppContext";
 
 const Login = () => {
-  const { setShowUserLogin } = useAppContext();
+  const { setShowUserLogin, setUser } = useAppContext();
   const [state, setState] = React.useState("login");
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setUser({
+      email: "test@pankajsharma.dev",
+      name: "PankajSharma",
+    });
+  };
+
   return (
     <div
       onClick={() => setShowUserLogin(false)}
       className="fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center text-sm text-gray-600 bg-black/50"
     >
       <form
+        onSubmit={onSubmitHandler}
         onClick={(e) => e.stopPropagation()}
         className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] text-gray-500 rounded-lg shadow-xl border border-gray-200 bg-white"
       >
