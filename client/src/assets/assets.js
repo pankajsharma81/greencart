c012c5e05d671b1f76d24a28572d84357eb3f7a9
pankajsharma81 <pankajsharma88279@gmail.com>
2025-11-23 12:33:11 +0530

    Update Orders component to fetch and display order details with improved layout

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -786,9 +786,9 @@ export const dummyAddress = [
   {
     _id: "67b5b9e54ea97f71bbc196a0",
     userId: "67b5880e4d09769c5ca61644",
-    firstName: "Great",
-    lastName: "Stack",
-    email: "user.greatstack@gmail.com",
+    firstName: "Pankaj",
+    lastName: "Sharma",
+    email: "test@gmail.com",
     street: "Street 123",
     city: "Main City",
     state: "New State",
